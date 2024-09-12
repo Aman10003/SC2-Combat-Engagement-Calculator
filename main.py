b6f75de9d98e14e5c5fd8ee06bb363edffe556a4
Anthony J Mangino <53193798+Aman10003@users.noreply.github.com>
2024-09-11 23:31:58 -0400

    A
    A

---
+++ main.py
@@ -0,0 +1,11 @@
+import units
+import numpy as np
+
+class functions:
+
+
+    def main(self):
+        x=1
+        
+if __name__ == "__main__":
+    function.main()
---
+++ units.py
@@ -0,0 +1,6 @@
+class unit:
+    def __init__(self, armor, health, shields, attr):
+        self.armor = armor
+        self.hp = health
+        self.shield = shields
+        self.attributes = attr
