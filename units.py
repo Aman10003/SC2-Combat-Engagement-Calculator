0425494adddc9e54d296437fb2f7aa0d21778ab4
Anthony J Mangino <53193798+Aman10003@users.noreply.github.com>
2024-09-12 12:10:01 -0400

    attr

--- units.py
+++ units.py
@@ -1,5 +1,16 @@
+# class attribute:
+#     def __init__(self, l = False, arm = False, mass = False, bio = False, mech = False, psi = False, structure = False, heroic = False):
+#         self.light = l
+#         self.armored = arm
+#         self.massive = mass
+#         self.bio = bio
+#         self.mech = mech
+#         self.psi = psi
+#         self.structure = structure
+#         self.heroic = heroic
+
 class unit:
-    def __init__(self, armor, health, shields, attr):
+    def __init__(self, armor, health, shields = 0, attr={"light" : False, "armored" : False, "massive" : False, "bio" : False, "mech" : False, "psi" : False, "structure" : False, "heroic" : False, "gnd" : False, "race": None}):
         self.armor = armor
         self.hp = health
         self.shield = shields
